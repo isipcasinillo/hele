50f25b1180091cf835fe0207deaee86678c0548e
isipcasinillo <isipcasinillo@gmail.com>
2022-03-09 14:58:16 -0600

    static files are served

--- server/server.js
+++ server/server.js
@@ -28,7 +28,11 @@ server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(
+  '/manifest.json',
+  express.static(path.join(__dirname, 'build', 'manifest.json'))
+);
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
@@ -36,7 +40,7 @@ if (process.env.NODE_ENV === 'production') {
 
 // sends all other request to the home page
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/public/index.html'));
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 db.once('open', () => {
